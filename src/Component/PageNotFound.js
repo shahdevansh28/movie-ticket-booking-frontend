50a0f9973905638f11fb51979915216237b005fe
Devansh Shah <86820517+shahdevansh28@users.noreply.github.com>
2024-03-27 09:26:24 +0530

    modified: src/Component/PageNotFound.js

--- src/Component/PageNotFound.js
+++ src/Component/PageNotFound.js
@@ -1,17 +1,32 @@
-import { useRouteError } from "react-router-dom";
+import { FitScreen } from "@mui/icons-material";
+import { Button, Container, Grid, Typography } from "@mui/material";
+import React from "react";
+import Footer from "./Footer";
+import { Link } from "react-router-dom";
 
 export default function PageNotFound() {
-  const error = useRouteError();
-
   return (
-    <div id="error-page" className="h-screen w-screen flex justify-center items-center">
-      <div>
-        <h1 className="text-5xl font-bold">Oops!</h1>
-        <p className="my-2 text-gray-600 text-lg">Sorry, an unexpected error has occurred.</p>
-        <p className="font-bold text-red-500">
-          <i>{error.statusText || error.message}</i>
-        </p>
-      </div>
-    </div>
+    <>
+      <Container>
+        <Grid container alignItems="center">
+          <img src="bg-img/404.png" height="720px" />
+          <Typography variant="h4" align="center" padding={10}>
+            Page Not Found
+            <br />
+            <Button
+              style={{
+                borderRadius: 30,
+                backgroundColor: "#FFBF00",
+              }}
+              variant="outline"
+            >
+              <Link to="/">Go to Home</Link>
+            </Button>
+          </Typography>
+        </Grid>
+      </Container>
+
+      <Footer />
+    </>
   );
 }
