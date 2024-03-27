ffc88b0b914e20ca3fcf44b07fb4e01be030e747
Devansh Shah <86820517+shahdevansh28@users.noreply.github.com>
2024-03-27 10:45:42 +0530

    modified: API.js

--- src/API.js
+++ src/API.js
@@ -1,1 +1,2 @@
-export const baseURL = "https://localhost:44397";
+// export const baseURL = "https://localhost:44397";
+export const baseURL = "https://movie-ticket-booking-backend-production.up.railway.app";
